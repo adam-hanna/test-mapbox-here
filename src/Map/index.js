1a1880a1ca7bd7d1f4e256ac94848550bae166d3
Adam Hanna <ahanna@alumni.mines.edu>
2019-12-11 03:15:38 +0000

    making progress

--- src/Map/index.js
+++ src/Map/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactMapGL from 'react-map-gl';
 import Dimensions from 'react-dimensions'
+import { fromJS } from 'immutable'
 import 'mapbox-gl/dist/mapbox-gl.css'
-import { retrieveToken } from '../utils/hereToken'
 
 const { 
-  REACT_APP_HERE_MAPS_ACCESS_KEY_ID: key,
-  REACT_APP_HERE_MAPS_ACCESS_KEY_SECRET: secret,
+  REACT_APP_HERE_MAPS_FREMIUM_TOKEN
 } = process.env
+console.log('REACT_APP_HERE_MAPS_FREMIUM_TOKEN', REACT_APP_HERE_MAPS_FREMIUM_TOKEN)
 
 export const Map = Dimensions({ elementResize: true })(({ containerWidth, containerHeight, viewport, onViewportChange, children }) => {
   const { zoom, latitude, longitude } = viewport
@@ -20,65 +20,71 @@ export const Map = Dimensions({ elementResize: true })(({ containerWidth, contai
   const yTile = Math.round(n * (1-(Math.log(Math.tan(latRad) + 1/Math.cos(latRad)) /Math.PI)) / 2)
 
   // note: https://uber.github.io/react-map-gl/#/Documentation/getting-started/about-mapbox-tokens
-  const transformRequest = async (url, resourceType) => {
-    // note: this needs to be in a try / catch
-    const token = await retrieveToken({ key, secret })
-
-    return {
-      url: url,
-      mode: 'no-cors',
-      headers: { 
-        'Authorization': 'Bearer ' + token
-      },
+  const transformRequest = (url, resourceType) => {
+    if (resourceType === 'Source' && url.match('vector.hereapi.com')) {
+      console.log('match')
+      return {
+        url,
+        mode: 'cors',
+        headers: { 
+          'Authorization': `Bearer ${REACT_APP_HERE_MAPS_FREMIUM_TOKEN}`,
+        },
+      }
     }
   }
 
   // note: https://docs.mapbox.com/mapbox-gl-js/style-spec/
-  /*
-  const mapStyle = {
-    "version": 1,
+  const mapStyle = fromJS({
+    "id": "here-map-basic",
+    "version": 8,
     "name": "Here Maps",
     "sources": {
       "here-map": {
         "type": "vector",
         "url": `https://vector.hereapi.com/v2/vectortiles/base/mc/${zoom}/${xTile}/${yTile}/omv`,
-        "maxzoom": 17,
-        "minzoom": 0
+      },
+      "osm": {
+        "type": "raster",
+        "tiles": [
+          "http://tile.openstreetmap.org/{z}/{x}/{y}.png"
+        ],
+        "minzoom": 0,
+        "maxzoom": 14
       }
     },
-    "layers": []
+    "layers": [
+      {
+        "id": "osm",
+        "type": "raster",
+        "source": "osm"
+      }
+    ]
+  })
+
+  /*
+  {
+    "id": "here-vector",
+    "type": "vector",
+    "source": "here-map",
+    "minzoom": 0,
+    "maxzoom": 17
   }
   */
 
-  //const mapStyle = {
-    //"version": 1,
-    //"name": "Here Maps",
-    //"sources": {
-      //[>
-      //"here-map": {
-        //"type": "raster",
-        //"tileSize": 512,
-        //// "url": `https://${loadBalancer}.base.maps.api.here.com/maptile/2.1/maptile/newest/normal.day/${zoom}/${xTile}/${yTile}/512/png?&app_id=${REACT_APP_HERE_MAPS_APP_ID}&app_code=${REACT_APP_HERE_MAPS_APP_CODE}`,
-        //"tiles": [
-          ////`https://${loadBalancer}.base.maps.api.here.com/maptile/2.1/maptile/newest/normal.day/${zoom}/${xTile}/${yTile}/512/png?&app_id=${REACT_APP_HERE_MAPS_APP_ID}&app_code=${REACT_APP_HERE_MAPS_APP_CODE}`
-          //// `https://${loadBalancer}.base.maps.api.here.com/maptile/2.1/maptile/newest/normal.day/{z}/{x}/{y}/512/png?&app_id=${REACT_APP_HERE_MAPS_APP_ID}&app_code=${REACT_APP_HERE_MAPS_APP_CODE}`
-        //]
-      //}
-      //*/
-      //"here-map": {
-        //"type": "vector",
-        //"url": `https://vector.hereapi.com/v2/vectortiles/base/mc/${zoom}/${xTile}/${yTile}/omv`,
-        //"maxzoom": 17,
-        //"minzoom": 0
-      //}
-    //},
-    //"layers": []
-  //}
-  // const mapStyle = `https://${loadBalancer}.base.maps.api.here.com/maptile/2.1/maptile/newest/normal.day/${zoom}/${xTile}/${yTile}/512/png?&app_id=${REACT_APP_HERE_MAPS_APP_ID}&app_code=${REACT_APP_HERE_MAPS_APP_CODE}`
-  //const mapStyle = `https://${loadBalancer}.base.maps.api.here.com/maptile/2.1/maptile/newest/normal.day/{z}/{x}/{y}/512/png?&app_id=${REACT_APP_HERE_MAPS_APP_ID}&app_code=${REACT_APP_HERE_MAPS_APP_CODE}`
+  /*
+  {
+    "id": "water",
+    "source": "here-map",
+    "source-layer": "water",
+    "type": "fill",
+    "paint": {
+      "fill-color": "#00ffff"
+    }
+  } 
+  */
 
-  const mapStyle = `https://vector.hereapi.com/v2/vectortiles/base/mc/${zoom}/${xTile}/${yTile}/omv`
-  // mapboxApiAccessToken={'pk.eyJ1IjoidGVsZWZvbmljYTQ2IiwiYSI6ImNrM3RnOXgyNzAya2oza3BscTZhd2s5amsifQ.6y16_yGzQZyzYRx-23QhZw'}
+  // const mapStyle = `https://vector.hereapi.com/v2/vectortiles/base/mc/${zoom}/${xTile}/${yTile}/omv`
+  // const mapStyle = "https://s3.amazonaws.com/cdn.brianbancroft.io/assets/osmstyle.json"
   return (
     <ReactMapGL
       width={containerWidth}
